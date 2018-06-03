ac49e552478d2d8cfcad17f5a5ffe19adda51304
Tom Gidden <tom@gidden.net>
2018-06-03 12:24:23 +0100

    blind2 changes

--- blind2_proxy.py
+++ blind2_proxy.py
@@ -116,44 +116,44 @@ if __name__ == '__main__':
 
     print "Starting"
 
-    try:
-        # For some reason, systemd passes HUP to the job when starting, so we
-        # have to ignore HUP.  Then it has trouble with INT, so best to
-        # explicitly exit on INT, and configure systemd to use INT.
-        def shutdown_handler(signum, frame):
-            print "Setting running to False"
-            shutdown_handler.running = False
+    while True:
+        try:
+            # For some reason, systemd passes HUP to the job when starting, so we
+            # have to ignore HUP.  Then it has trouble with INT, so best to
+            # explicitly exit on INT, and configure systemd to use INT.
+            def shutdown_handler(signum, frame):
+                print "Setting running to False"
+                shutdown_handler.running = False
 
-        shutdown_handler.running = True
-        signal.signal(signal.SIGINT, shutdown_handler)
+            shutdown_handler.running = True
+            signal.signal(signal.SIGINT, shutdown_handler)
 
-        # Ignore HUP
-        def ignore_handler(signum, frame):
-            pass
-        signal.signal(signal.SIGHUP, ignore_handler)
+            # Ignore HUP
+            def ignore_handler(signum, frame):
+                pass
+            signal.signal(signal.SIGHUP, ignore_handler)
 
-        # Find Arduino Nano fake
-        dev = [port.device for port in serial.tools.list_ports.comports() if port.vid==0x1a86 and port.pid==0x7523]
-        ser = serial.serial_for_url(dev[0], baudrate=115200)
-        t = serial.threaded.ReaderThread(ser, serialProxy)
-        t.start()
+            # Find Arduino Nano fake
+            dev = [port.device for port in serial.tools.list_ports.comports() if port.vid==0x1a86 and port.pid==0x7523]
+            ser = serial.serial_for_url(dev[0], baudrate=115200)
+            t = serial.threaded.ReaderThread(ser, serialProxy)
+            t.start()
 
-        mqtt_client = mqttClient(mqtt=mqtt)
-        mqtt_client.daemon = True
-        mqtt_client.start()
+            mqtt_client = mqttClient(mqtt=mqtt)
+            mqtt_client.daemon = True
+            mqtt_client.start()
 
-        while shutdown_handler.running:
-            time.sleep(1)
-        sys.exit(0)
+            while shutdown_handler.running:
+                time.sleep(1)
+            sys.exit(0)
 
-    except (KeyboardInterrupt, SystemExit) as e:
-        print e
-        sys.exit(1)
+        except (KeyboardInterrupt, SystemExit) as e:
+            print e
+            sys.exit(0)
 
-#    except OSError as e:
-#        t.reconnect()
+    #    except OSError as e:
+    #        t.reconnect()
 
-    except Exception as e:
-        print e
-        pass
-
+        except Exception as e:
+            print e
+            sys.exit(1)
